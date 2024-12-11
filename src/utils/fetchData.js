b4b0be3b2af4dcbeafd207012010b702228e3963
Emelie Hofland <emelie_hofland@hotmail.com>
2024-12-11 20:01:21 +0100

    Added Auth0 and navbar

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,8 +1,5 @@
-// app/fetchData.js
-"use server"; // This directive indicates that this file will only be executed on the server
-
+// Function to call openfoodfacts
 export async function fetchData(result) {
-    "use server";
     const url = `https://world.openfoodfacts.org/api/v3/product/${result}.json`;
     console.log("Fetching data from:", url);
 
